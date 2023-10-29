cc0baa487c1a82fa942aeb556cc168be437250ff
Venky Gangisetti <venkateshgangisetti@outlook.com>
2023-10-28 22:23:11 -0500

    reset migrations

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from django.test import LiveServerTestCase
 from selenium.common.exceptions import WebDriverException
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 import time
 
 MAX_WAIT = 10
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
     def tearDown(self):
